ed86fe97dc2b7df2181799cc04217d0311dddd5d
Eliana Del Mar Velásquez Rodriguez <103125856+MeerVelasquez@users.noreply.github.com>
2023-04-27 08:44:36 -0500

    Tentative API with Frontend 2

--- apli/ecgpt/views.py
+++ apli/ecgpt/views.py
@@ -4,7 +4,7 @@ import openai
 
 
 openai.api_key = "#"
-model_engine = "text-davinci-002"
+model_engine = "davinci"
 
 
 def index(request):
@@ -25,3 +25,7 @@ def chatbot(request):
         reply = response.choices[0].text.strip()
         return render(request, 'ecgpt/chatbot.html', {'question': prompt, 'answer': reply})
     return render(request, 'ecgpt/chatbot.html')
+
+
+def dashboard(request):
+    return render(request, 'ecgpt/dashboard.html')
